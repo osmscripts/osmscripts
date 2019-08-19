97686b83775e98bdea85d07062c35875a7fd2a6f
Vladislav Osmianskij <vlaosm@gmail.com>
2019-08-19 10:27:56 +0300

    `osmscripts create:script --runner` option; `phpunit` script runner

--- templates/osmscripts/runner.php
+++ templates/osmscripts/runner.php
@@ -3,7 +3,7 @@
 
 use OsmScripts\Core\Hints\ComposerLockHint;
 
-function script_runner() {
+call_user_func(function () {
     $name = basename(__FILE__);
     if (!is_file('composer.lock') || !($contents = file_get_contents('composer.lock'))) {
         throw new Exception("'composer.lock' not found");
@@ -14,17 +14,18 @@ function script_runner() {
         throw new Exception("'composer.lock' is not valid JSON file");
     }
 
-    foreach ($json->packages ?? [] as $package) {
-        foreach ($package->bin as $script) {
-            if (basename($script) === $name) {
-                /** @noinspection PhpIncludeInspection */
-                include "vendor/{$package->name}/{$script}";
-                return;
+    foreach (['packages', 'packages-dev'] as $packages) {
+        foreach ($json->$packages ?? [] as $package) {
+            foreach ($package->bin ?? [] as $script) {
+                if (basename($script) === $name) {
+                    /** @noinspection PhpIncludeInspection */
+                    include "vendor/{$package->name}/{$script}";
+
+                    return;
+                }
             }
         }
     }
 
     throw new Exception("Script '$name' is not found in '" . getcwd() . "'");
-}
-
-script_runner();
+});
