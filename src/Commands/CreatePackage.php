2e4f43d595dde8cc833a7d8f5225a3efcdc2f444
Vladislav Osmianskij <vlaosm@gmail.com>
2019-08-19 08:23:31 +0300

    adding '-g' option to `osmscripts create:package`

--- src/Commands/CreatePackage.php
+++ src/Commands/CreatePackage.php
@@ -4,6 +4,8 @@ namespace OsmScripts\OsmScripts\Commands;
 
 use Exception;
 use OsmScripts\Core\Commands\CreatePackage as BaseCreatePackage;
+use OsmScripts\Core\Script;
+use Symfony\Component\Console\Input\InputOption;
 
 /** @noinspection PhpUnused */
 
@@ -12,23 +14,33 @@ use OsmScripts\Core\Commands\CreatePackage as BaseCreatePackage;
  */
 class CreatePackage extends BaseCreatePackage
 {
-    public $expect_current = true;
-
     protected function configure() {
         parent::configure();
         $this
             ->setDescription("Creates new Composer package for writing script commands, " .
                 "pushes it to the specified Git repo and installs it locally")
             ->setHelp(<<<EOT
-Run this command from {$this->script_path}.
-
 Before running this command create empty repo on GitHub or other Git hosting provider. 
 Pass URL of Git repo using --repo_url=REPO_URL syntax.
 
 Also, before running this command commit and push all changes in all the packages in `vendor`.
 directory.
 EOT
-            );
+            )
+            ->addOption('global', '-g', InputOption::VALUE_NONE,
+                "If set, package is created in global Composer installation rather " .
+                "than in current directory.");
+    }
+
+    protected function handle() {
+        /* @var Script $script */
+        global $script;
+
+        if ($this->input->getOption('global')) {
+            $script->workGlobally();
+        }
+
+        parent::handle();
     }
 
     protected function createPackage() {
