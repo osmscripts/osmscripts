a15abdcdeb086cb67e07fc257541b55ae15c684a
Vladislav Osmianskij <vlaosm@gmail.com>
2019-08-27 10:55:37 +0300

    `osmscripts -g packages` and `osmscripts -g scripts` commands

---
+++ src/Commands/Packages.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace OsmScripts\OsmScripts\Commands;
+
+use OsmScripts\Core\Command;
+use OsmScripts\Core\Project;
+use OsmScripts\Core\Script;
+
+/** @noinspection PhpUnused */
+
+/**
+ * `packages` shell command class.
+ *
+ * @property Project $project @required
+ */
+class Packages extends Command
+{
+    #region Properties
+    public function __get($property) {
+        /* @var Script $script */
+        global $script;
+
+        switch ($property) {
+            case 'project': return $this->project = new Project(['path' => $script->cwd]);
+        }
+
+        return null;
+    }
+    #endregion
+
+    protected function configure() {
+        $this->setDescription("Lists installed packages");
+    }
+
+    protected function handle() {
+        foreach ($this->project->packages as $package) {
+            $this->output->writeln($package->name);
+        }
+    }
+}
---
+++ src/Commands/Scripts.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace OsmScripts\OsmScripts\Commands;
+
+use OsmScripts\Core\Command;
+use OsmScripts\Core\Project;
+use OsmScripts\Core\Script;
+
+/** @noinspection PhpUnused */
+
+/**
+ * `scripts` shell command class.
+ *
+ * @property Project $project @required
+ */
+class Scripts extends Command
+{
+    #region Properties
+    public function __get($property) {
+        /* @var Script $script */
+        global $script;
+
+        switch ($property) {
+            case 'project': return $this->project = new Project(['path' => $script->cwd]);
+        }
+
+        return null;
+    }
+    #endregion
+
+    protected function configure() {
+        $this->setDescription("Lists installed scripts and packages they are defined in");
+    }
+
+    protected function handle() {
+        foreach ($this->project->packages as $package) {
+            foreach ($package->lock->bin ?? [] as $script) {
+                $this->output->writeln(sprintf("%-20s%s", basename($script), $package->name));
+            }
+        }
+    }
+}
